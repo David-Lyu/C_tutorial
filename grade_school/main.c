f4f0a9c84525c6fb61f8e13377043e15382057ea
David-Lyu <lyu.david@yahoo.com>
2024-03-31 12:19:08 -0700

    added files and readme

---
+++ grade_school/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/*
+Students
+Grades
+
+We need a file to store and write too
+CRUD
+We want a Student Table: student_id, name, course_id
+We want a Grades Table: grades, student_id, course_id
+We want a Course Table: course, student_id
+
+*/
+
+int main(int argc, char const *argv[]) {
+    /* code */
+
+    int userChoice = 0;
+
+    //grab first menu
+        // access student 
+            // create students
+                // name
+            // update students
+                // name, course_id,
+            // delete students
+                // delte student by id
+            // read students
+                //student get grades, course
+    
+        // access courses
+            // set students
+
+        // access grades
+            // get student_id, grades, and course_id
+
+    // set grades in courses
+    do {
+      userChoice = 0;  
+    }
+    while (userChoice != 0);
+    return 0;
+}
---
+++ lisp_tutorial/README.md
@@ -0,0 +1,2 @@
+Document to find this tutorial can be found here:
+https://www.buildyourownlisp.com/
